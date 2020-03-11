7b7183305f11a721b9369c71f7ed3da8e7242de1
jalal246 <jimmy002020@gmail.com>
2020-03-11 20:40:18 +0200

    fix files export, remove legacy.

--- src/deps.js
+++
@@ -1,4 +0,0 @@
-export { default as textics } from 'textics';
-export { default as util } from 'util';
-export { default as stream } from 'stream';
-export { default as EventEmitter } from 'events';
